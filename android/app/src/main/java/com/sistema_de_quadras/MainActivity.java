8a834cb843424d7b04f03b8d3a195761a8892fed
deniscapp <denis_cappelini@hotmail.com>
2017-10-24 19:17:42 -0200

    first commit

---
+++ android/app/src/main/java/com/sistema_de_quadras/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sistema_de_quadras;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sistema_de_quadras";
+    }
+}
